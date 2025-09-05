df6d1c0141d2a8e2184fd76844515f30d6ab0582
Nahuel Cassinari <matandomuertos@users.noreply.github.com>
2025-09-05 14:59:52 +0200

    add cloudfront

--- tofu/cloudflare/dns.tf
+++ tofu/cloudflare/dns.tf
@@ -15,3 +15,23 @@ resource "cloudflare_dns_record" "main_domain" {
   content = var.koko_ip
   proxied = false
 }
+
+resource "cloudflare_dns_record" "all_private" {
+  zone_id = cloudflare_zone.wyppu.id
+  name    = "*"
+  ttl     = 1 #auto
+  type    = "A"
+  comment = "All private subdomains"
+  content = var.koko_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "cname" {
+  zone_id = cloudflare_zone.wyppu.id
+  name    = "www"
+  ttl     = 1 #auto
+  type    = "CNAME"
+  comment = "CNAME for www"
+  content = var.zone_name
+  proxied = true
+}
