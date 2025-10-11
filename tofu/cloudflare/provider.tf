aa66c0f7068917e4c8da9747e5b249b9cac0f2ed
Nahuel Cassinari <matandomuertos@users.noreply.github.com>
2025-10-11 18:40:34 +0200

    move statefile to cf

--- tofu/cloudflare/provider.tf
+++ tofu/cloudflare/provider.tf
@@ -10,15 +10,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket                      = "tf-bucket"
-    region                      = "auto"
-    key                         = "state/cloudflare/opentofu.tfstate"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    use_path_style              = true
+    key = "state/cloudflare/opentofu.tfstate"
   }
 }
 
