0e7382e6c819a9ba86cd1a480a06b7fa51447b4d
Nahuel Cassinari <matandomuertos@users.noreply.github.com>
2025-10-11 16:29:05 +0200

    move statefile to cf

--- tofu/cloudflare/provider.tf
+++ tofu/cloudflare/provider.tf
@@ -1,3 +1,5 @@
+# tofu apply -var-file=/bkp/tofu/cloudflare/terraform.tfvars
+
 terraform {
   required_providers {
     cloudflare = {
@@ -6,8 +8,14 @@ terraform {
     }
   }
 
-  backend "local" {
-    path = "/bkp/tofu/cloudflare/terraform.tfstate"
+  backend "s3" {
+    bucket                      = "tf-bucket"
+    region                      = "EEUR"
+    endpoint                    = "https://${cloudflare_account_id}.r2.cloudflarestorage.com"
+    key                         = "state/${path_relative_to_include()}/opentofu.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
   }
 }
 
