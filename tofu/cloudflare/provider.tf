c7ea65630f8f7de4b1f5b982cdf7492188793cb5
Nahuel <matandomuertos@users.noreply.github.com>
2025-10-11 15:49:37 +0000

    update cloudflare backend

--- tofu/cloudflare/provider.tf
+++ tofu/cloudflare/provider.tf
@@ -10,15 +10,14 @@ terraform {
 
   backend "s3" {
     bucket                      = "tf-bucket"
-    region                      = "EEUR"
-    key                         = "state/${path_relative_to_include()}/opentofu.tfstate"
+    region                      = "auto"
+    key                         = "state/cloudfare/opentofu.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    force_path_style            = true
-
-    endpoint = {
-      s3 = "https://${cloudflare_account_id}.r2.cloudflarestorage.com"
-    }
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
 
