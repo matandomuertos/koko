0684a703360558f0cd8cc0364e25ca31f6ec6a3a
Nahuel User <matandomuertos@users.noreply.github.com>
2025-10-27 23:25:10 +0000

    add pve2

--- tofu/proxmox_base/storage.tf
+++ tofu/proxmox_base/storage.tf
@@ -1,4 +1,11 @@
-# I didn't find the way to do it in tf
-# pvesm add dir iso --path /iso --content iso,import --shared 0
+## I didn't find the way to do it in tf
+
+## KOKO-PVE1
+# pvesm add dir iso --path /iso --content iso,import --shared 1
 # pvesm add dir data --path /data --content images,rootdir --is_mountpoint 1 --shared 1
 # pvesm add dir data-hdd --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --shared 1
+
+## KOKO-PVE2
+# pvesm add dir iso2 --path /iso --content iso,import --shared 1
+# pvesm add dir data2 --path /data --content images,rootdir --is_mountpoint 1 --shared 1
+# pvesm add dir data-hdd2 --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --shared 1
