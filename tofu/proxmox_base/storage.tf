94b5de808edd2e66c18d35018ad5d88665b47f98
Nahuel User <matandomuertos@users.noreply.github.com>
2025-10-29 10:54:21 +0000

    fix volumes

--- tofu/proxmox_base/storage.tf
+++ tofu/proxmox_base/storage.tf
@@ -1,11 +1,11 @@
 ## I didn't find the way to do it in tf
 
 ## KOKO-PVE1
-# pvesm add dir iso --path /iso --content iso,import --shared 1
-# pvesm add dir data --path /data --content images,rootdir --is_mountpoint 1 --shared 1
-# pvesm add dir data-hdd --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --shared 1
+# pvesm add dir iso --path /iso --content iso,import --nodes koko-pve1 --shared 0
+# pvesm add dir data --path /data --content images,rootdir --is_mountpoint 1 --nodes koko-pve1 --shared 0
+# pvesm add dir data-hdd --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --nodes koko-pve1 --shared 0
 
 ## KOKO-PVE2
-# pvesm add dir iso2 --path /iso --content iso,import --shared 1
-# pvesm add dir data2 --path /data --content images,rootdir --is_mountpoint 1 --shared 1
-# pvesm add dir data-hdd2 --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --shared 1
+# pvesm add dir iso2 --path /iso --content iso,import --nodes koko-pve2 --shared 0
+# pvesm add dir data2 --path /data --content images,rootdir --is_mountpoint 1 --nodes koko-pve2 --shared 0
+# pvesm add dir data-hdd2 --path /data-hdd --content images,rootdir,backup --is_mountpoint 1 --nodes koko-pve2 --shared 0
