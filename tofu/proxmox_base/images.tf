b0fde1fea02740d3ec6fcb2e00d6f1fed63eedaf
Nahuel User <matandomuertos@users.noreply.github.com>
2025-10-29 13:59:33 +0000

    add pbs iso

--- tofu/proxmox_base/images.tf
+++ tofu/proxmox_base/images.tf
@@ -5,3 +5,11 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   file_name    = "ubuntu2204-amd64.qcow2"
 }
+
+resource "proxmox_virtual_environment_download_file" "proxmox_backup_server" {
+  content_type = "iso"
+  datastore_id = "iso2"
+  node_name    = "koko-pve2"
+  url          = "https://enterprise.proxmox.com/iso/proxmox-backup-server_4.0-1.iso"
+  file_name    = "pbs-4.0-1.iso"
+}
